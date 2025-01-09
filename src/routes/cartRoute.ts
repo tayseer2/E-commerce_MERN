806b611411e72e0e0a2f991a5f3d802b05a1c3ae
tayseer2 <tysyrt200@gmail.com>
2025-01-09 23:42:13 +0300

    Checkout logic / route service model

--- src/routes/cartRoute.ts
+++ src/routes/cartRoute.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import {
   addItemToCart,
+  checkout,
   clearCart,
   deleteItemInCart,
   getActiveCartForUser,
@@ -47,5 +48,12 @@ router.put(
     res.status(response.statusCode).send(response.data);
   }
 );
+
+router.post("/checkout", validateJWT, async (req: ExtendedRequest, res) => {
+  const userId = req?.user?._id;
+  const { address } = req.body;
+  const response = await checkout({ userId, address });
+  res.status(response.statusCode).send(response.data);
+});
 
 export default router;
--- src/services/cartService.ts
+++ src/services/cartService.ts
@@ -1,4 +1,5 @@
 import { cartModel } from "../models/cartModel";
+import { IOrderItem, orderModel } from "../models/orderModel";
 import productModel from "../models/productModel";
 
 interface CreateCartForUser {
@@ -175,4 +176,54 @@ export const deleteItemInCart = async ({
   const updatedCart = await cart.save();
 
   return { data: updatedCart, statusCode: 200 };
+};
+
+interface Checkout {
+  userId: string;
+  address: string;
+}
+
+export const checkout = async ({ userId, address }: Checkout) => {
+  if (!address) {
+    return { data: "Address is required!", statusCode: 400 };
+  }
+
+  const cart = await getActiveCartForUser({ userId });
+
+  const orderItems: IOrderItem[] = [];
+
+  for (const item of cart.items) {
+    const product = await productModel.findById(item.product);
+
+    if (!product) {
+      return { data: "Product not found!", statusCode: 404 };
+    }
+
+    const orderItem: IOrderItem = {
+      productTitle: product.title,
+      productImage: product.image,
+      quantity: item.quantity,
+      unitPrice: item.unitPrice,
+      push: function (orderItems: IOrderItem): unknown {
+        throw new Error("Function not implemented.");
+      },
+    };
+
+    orderItems.push(orderItem);
+  }
+
+  const order = await orderModel.create({
+    orderItems,
+    total: cart.totalAmount,
+    address,
+    userId,
+  });
+
+  await order.save();
+
+  // Update the cart Status to be completed
+  cart.status = "completed";
+  await cart.save();
+
+  return { data: order, statusCode: 200 };
 };
