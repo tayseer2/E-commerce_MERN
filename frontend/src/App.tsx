8e208a98a23e0da9701c4e7ca60fc13c1a084262
tayseer2 <tysyrt200@gmail.com>
2025-01-11 17:54:44 +0300

    All products in page Home  + integration with API

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-import Navbar from "./components/NavBar";
+import Navbar from "./components/Navbar";
+
 
 function App() {
   return (
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,8 +1,44 @@
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import ProductCard from "../components/ProductCard";
+import { useEffect, useState } from "react";
+import { Product } from "../types/product";
+import { BASE_URL } from "../constants/baseUrl";
+import  Alert  from "@mui/material/Alert";
 
 export default function HomePage() {
+  const [products, setproducts] = useState<Product[]>([]);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+
+      const fetchData = async () => {
+        try {
+          const response = await fetch(`${BASE_URL}/product`);
+          const data = await response.json();
+          setproducts(data);
+
+        } catch  {
+            setError(true)
+        }
+      }
+
+      fetchData()
+  }, []);
+
+  if(error) {
+    return <Alert severity="error">Something went wrong!</Alert>
+  }
+
   return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
+    <Container sx={{ mt: 4 }}>
+      <Grid container spacing={2}>
+        {products.map((p, index) => (
+          <Grid key={index} item md={4}>
+            <ProductCard {...p} />
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
   );
 }
