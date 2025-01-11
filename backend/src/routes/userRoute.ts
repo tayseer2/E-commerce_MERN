0b348e0ad0d519caef924e9e624a1a08081f346d
tayseer2 <tysyrt200@gmail.com>
2025-01-11 21:11:02 +0300

    Register Page

--- backend/src/routes/userRoute.ts
+++ backend/src/routes/userRoute.ts
@@ -12,7 +12,7 @@ router.post("/register", async (req, res) => {
       email,
       password,
     });
-    res.status(statusCode).send(data);
+    res.status(statusCode).json(data);
   } catch (err) {
     res.status(500).send("Something Went Wrong!");
   }
@@ -22,7 +22,7 @@ router.post("/login", async (req, res) => {
   try {
     const { email, password } = req.body;
     const { statusCode, data } = await login({ email, password });
-    res.status(statusCode).send(data);
+    res.status(statusCode).json(data);
   } catch (err) {
     res.status(500).send("Something Went Wrong!");
   }
---
+++ frontend/src/pages/RegisterPage.tsx
@@ -0,0 +1,95 @@
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import { useRef, useState } from "react";
+import { BASE_URL } from "../constants/baseUrl";
+import Alert from "@mui/material/Alert";
+
+export default function RegisterPage() {
+  const [error, seterror] = useState("");
+
+  const firstNameRef = useRef<HTMLInputElement>(null);
+  const lastNameRef = useRef<HTMLInputElement>(null);
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+
+  const onSubmit = async () => {
+    const firstName = firstNameRef.current?.value;
+    const lastName = lastNameRef.current?.value;
+    const email = emailRef.current?.value;
+    const password = passwordRef.current?.value;
+
+    // Make the call to API to creat the user
+    const response = await fetch(`${BASE_URL}/user/register`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        firstName,
+        lastName,
+        email,
+        password,
+      }),
+    });
+
+    if (!response.ok) {
+      seterror("Unaple to register user, please try agin");
+      return;
+    }
+
+    const data = await response.json();
+
+    console.log(data);
+  };
+
+  return (
+    <Container>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          mt: 4,
+        }}
+      >
+        <Typography variant="h5">Register New Account</Typography>
+
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            gap: 2,
+            mt: 4,
+            width: "42%",
+          }}
+        >
+          <TextField
+            inputRef={firstNameRef}
+            label="First Name"
+            name="firstName"
+          />
+          <TextField inputRef={lastNameRef} label="Last Name" name="lastName" />
+          <TextField inputRef={emailRef} label="Email" name="email" />
+          <TextField
+            inputRef={passwordRef}
+            type="password"
+            label="Password"
+            name="password"
+          />
+          <Button onClick={onSubmit} variant="contained">
+            Register
+          </Button>
+          {error && (
+            <Alert variant="filled" severity="error">
+              {error}
+            </Alert>
+          )}
+        </Box>
+      </Box>
+    </Container>
+  );
+}
